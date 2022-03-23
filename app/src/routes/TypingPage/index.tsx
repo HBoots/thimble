cdf7527e71a755068190943156f8b314984f2113
Justin Dudley <justindudley99@gmail.com>
2022-03-23 12:51:56 -0400

    make screen height perfect on all phones by accessing innerHeight

--- app/src/routes/TypingPage/index.tsx
+++ app/src/routes/TypingPage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SampleSection } from './components/SampleSection';
 import { HomeLinkSection } from './components/HomeLinkSection';
 import { KeyboardAndConsoleLogicWrap } from './components/KeyboardAndConsoleLogicWrap';
@@ -6,6 +6,20 @@ import { KeyboardAndConsoleLogicWrap } from './components/KeyboardAndConsoleLogi
 import styles from './style.module.css';
 
 export const TypingPage: React.FC = () => {
+   // grok innerHeight so phone's nav bars and scroll bars don't affect proper screen height
+   const resetJumpVh = () => {
+      let jumpVh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--jump-vh', `${jumpVh}px`);
+   };
+
+   window.addEventListener('resize', () => {
+      resetJumpVh();
+   });
+
+   useEffect(() => {
+      resetJumpVh();
+   });
+
    return (
       <div className={styles.typingPageViewHeight}>
          <div className={styles.typingPage}>
