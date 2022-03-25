b542bcf02ea90d837ddfc384b4f421cc226e84fb
Justin Dudley <justindudley99@gmail.com>
2022-03-25 16:19:25 -0400

    get better crosshairs-panel image

--- app/src/constants/userChoices.ts
+++ app/src/constants/userChoices.ts
@@ -4,6 +4,8 @@ import TargetLogoHit from '../assets/images/target-logo-hit.svg';
 import TargetLogoMiss from '../assets/images/target-logo-miss.svg';
 import CrosshairsHit from '../assets/images/crosshairs-hit.png';
 import CrosshairsMiss from '../assets/images/crosshairs-miss.png';
+import CrosshairsPanel from '../assets/images/crosshairs-panel.png';
+
 import DotHit from '../assets/images/letter_D.png';
 import DotMiss from '../assets/images/letter_D.png';
 
@@ -27,7 +29,7 @@ export const medallions: Medallion[] = [
       images: {
          hit: CrosshairsHit,
          miss: CrosshairsMiss,
-         panel: CrosshairsHit,
+         panel: CrosshairsPanel,
       },
    },
    {
