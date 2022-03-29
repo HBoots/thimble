88ab3970baa88ebf12d635ebabcebe33aaba88c5
Justin Dudley <justindudley99@gmail.com>
2022-03-29 16:10:12 -0400

    routing for new page set up

---
+++ app/src/routes/ExplanationPage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './style.module.css';
+
+export const ExplanationPage: React.FC = () => {
+   return (
+      <div className={styles.test}>
+         Hello from Explanation! What IS Thimble, anyway??
+      </div>
+   );
+};
