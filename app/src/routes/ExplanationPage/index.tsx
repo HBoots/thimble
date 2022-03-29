6fd38dc7dc6c059515bbab71493a21a5924c03c3
Justin Dudley <justindudley99@gmail.com>
2022-03-29 17:22:25 -0400

    enable navigation through app, set up basic styling

--- app/src/routes/ExplanationPage/index.tsx
+++ app/src/routes/ExplanationPage/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { HomeLinkSection } from '../../components/HomeLinkSection';
 
 import styles from './style.module.css';
 
 export const ExplanationPage: React.FC = () => {
    return (
-      <div className={styles.test}>
-         Hello from Explanation! What IS Thimble, anyway??
-      </div>
+      <>
+         <HomeLinkSection />
+         <div className={styles.explanationPage}>
+            What IS Thimble, anyway?? Thimble is not about typing the correct
+            letter. In fact, Thimble doesn't even know if you've typed the
+            correct letter. But Thimble is very opinionated about WHERE, within
+            the key, your finger touched the screen.
+         </div>
+      </>
    );
 };
