291a5fc95e2533c5b987b40014974fc2611fb00d
Justin Dudley <justindudley99@gmail.com>
2022-03-30 16:00:54 -0400

    first 4 examples work. Last 2, with Checkbox Hack, do not

--- app/src/routes/TransitionTestingPage/index.tsx
+++ app/src/routes/TransitionTestingPage/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HomeLinkSection } from '../../components/HomeLinkSection';
+import { medallions } from '../../constants/userChoices';
 
 import styles from './style.module.css';
 
@@ -9,8 +10,33 @@ export const TransitionTestingPage: React.FC = () => {
          <HomeLinkSection />
          <div className={styles.transitionTestingPage}>
             <div> TRANSITION TESTING:</div>
-            <div className={styles.stretchBox}>stretch me on hover</div>
+            <div className={styles.stretch}>stretch me on hover</div>
             <div className={styles.rotation}>rotate me on hover</div>
+            <div className={styles.scale}>scale me on hover</div>
+            <div className={styles.parent}>
+               <div>parent</div>
+               <div className={styles.child}>child</div>
+            </div>
+            {/* The "label" hack:  https://www.delftstack.com/howto/css/css-onclick/ */}
+            <div>
+               <input type="checkbox" id="btn" />
+               <label htmlFor="btn">
+                  <img
+                     src={medallions[0].images.panel}
+                     alt="success medallion"
+                     // width={20}
+                  />
+               </label>
+            </div>
+            <div>
+               <label htmlFor="toggle">Toggle</label>
+               <input
+                  type="checkbox"
+                  id="toggle"
+                  className={styles.visuallyHidden}
+               />
+               <div className={styles.controlMe}></div>
+            </div>
          </div>
       </>
    );
